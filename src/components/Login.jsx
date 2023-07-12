7b84d942d2ad04779d7616ed143dc03a3b1cbb9e
Josie Parker <josieparker@Josies-MacBook-Pro.local>
2023-07-12 16:57:41 -0500

    login function is working, need to figure out what to do onlogin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginUserAPI } from "./api-adapter";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    //state for username and password
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginUser = async(username, password) => {
+        const response = await loginUserAPI(username, password);
+
+        console.log(response, "response from loginUser in login page!!!");
+
+        if (response?.success){
+            localStorage.setItem("token", response.token);
+            localStorage.setItem("username", username);
+            setTimeout(() => {
+                alert("you've been logged in!!");
+                navigate("/");
+            }, 3000);
+        }
+    }
+
     return (
         <div>
             <h2>login page!</h2>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                loginUser(username, password);
+            }} >
+                <label>username:</label>
+                <input type="text" onChange={(e) => {
+                    setUsername(e.target.value);
+                }} />
+                <label>password:</label>
+                <input type="text" onChange={(e) => {
+                    setPassword(e.target.value);
+                }}  />
+                <button type="submit">login</button>
+            </form>
         </div>
     )
 }
---
+++ src/components/api-adapter/index.js
@@ -0,0 +1,33 @@
+const URL = "http://localhost:4000";
+
+const makeHeaders = (token) => {
+    return{
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+    }
+}
+
+export const loginUserAPI = async(username, password) => {
+    try {
+        const response = await fetch(`${URL}/api/users/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        });
+
+        const result = await response.json();
+        
+        console.log(result, "result from loginAPI func!!");
+
+        return result;
+
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
